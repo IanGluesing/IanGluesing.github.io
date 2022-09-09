08d2e83586ce573f0cb73bb289e2df11d9dc1e15
Ian Gluesing <ian.gluesing@gmail.com>
2022-09-09 10:17:59 -0500

    Add correct rotation and position offsets, but coords are not working as expected

--- script.js
+++ script.js
@@ -25,9 +25,10 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', './magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('rotation', '0 0 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '.25 .25 .25');
+        model.setAttribute('scale', '.15 .15 .15');
+        model.setAttribute('position', { x: 0, y: 0, z: -10 });
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
